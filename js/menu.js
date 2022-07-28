e68fc90c786517f1cf005a0da30a179a0797b8d2
Vadym-Vasiliev <98331150+Vadym-Vasiliev@users.noreply.github.com>
2022-07-28 14:12:36 +0300

    add fix

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,8 @@
-const menuBtn = document.querySelector("[data-menu-btn]");
+const menuBtn = document.querySelector("[data-menu-button]");
 const menuBox = document.querySelector("[data-menu-box]");
 
 menuBtn.addEventListener("click", () => {
   menuBtn.classList.toggle("is-open");
   menuBox.classList.toggle("is-open");
-
   document.body.classList.toggle("menu-open");
 });
