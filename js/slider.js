d12a933de4d25792f33e560356879c60d700a15b
Vadym-Vasiliev <98331150+Vadym-Vasiliev@users.noreply.github.com>
2022-08-03 12:37:46 +0300

    add script  slider

---
+++ js/slider.js
@@ -0,0 +1,18 @@
+// let offset = 0; // зміщення від лівого краю
+// const reviewsListImages = document.querySelector(".reviews-list");
+
+// document.querySelector(".btn-next").addEventListener("click", function () {
+//   offset += 80;
+//   if (offset > 320) {
+//     offset = 0;
+//   }
+//   reviewsListImages.style.left = -offset + "px";
+// });
+
+// document.querySelector(".btn-prev").addEventListener("click", function () {
+//   offset -= 80;
+//   if (offset < 0) {
+//     offset = 0;
+//   }
+//   reviewsListImages.style.left = -offset + "px";
+// });
